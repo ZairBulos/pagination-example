148be12fb7cfd08bd28cbfcc912e430e7c1df604
ZairBulos <buloszair@gmail.com>
2023-10-26 17:37:11 -0300

    Fix cors

--- api/src/main/java/com/example/controllers/BookController.java
+++ api/src/main/java/com/example/controllers/BookController.java
@@ -2,9 +2,11 @@ package com.example.controllers;
 
 import com.example.entities.Book;
 import com.example.services.impl.BookServiceImpl;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("api/books")
 public class BookController extends BaseControllerImpl<Book, BookServiceImpl> {}
